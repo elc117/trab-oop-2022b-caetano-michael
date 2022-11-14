e93168e9584988b4139007a1e2e278ec7d1d84a6
Michael Strahl <michaelstrahl.sm@gmail.com>
2022-11-13 22:44:57 -0300

    init frontend

---
+++ scripts.js
@@ -0,0 +1,17 @@
+$(function() {
+  const accordions = document.querySelectorAll(".accordion-collapse");
+  let opening = false;
+  accordions.forEach(function (el) {
+    el.addEventListener("hide.bs.collapse", (event) => {
+      if (!opening) {
+        event.preventDefault();
+        event.stopPropagation();
+      } else {
+        opening = false;
+      }
+    });
+    el.addEventListener("show.bs.collapse", (event) => {
+      opening = true;
+    });
+  });
+})
